940937298082be1701f532dab342bc3cc554a0ba
Danilo Santos <danilo.santos0404@hotmail.com>
2023-07-04 15:47:26 -0300

    React router refresh problem fixed. Basename added to router in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <Navbar />
       <Routes>
         <Route path="/" element={<About />} />
